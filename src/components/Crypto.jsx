730a57a8c00dc5ced1f8ac1c9f275666ff716cfc
Jonathan Ruiz <jonathan.ruizz@hotmail.com>
2022-11-23 15:44:15 -0300

    Se creó la página para renderizar la información completa de cada cryptomoneda, tenemos un error en el .map al recorrer las crypto

--- src/components/Crypto.jsx
+++ src/components/Crypto.jsx
@@ -10,25 +10,25 @@ const Crypto = () => {
         // fetch(`${API_URL}assets`)
         // .then((res) => res.json()) // Convertimos la respuesta en un JSON
         axios.get(`${API_URL}assets`)
-        .then((data) => {
-            console.log(data.data.data) // La respuesta en json se llamará "data" e imprimímos la data
-            setCryptos(data.data.data)
-        })
-        .catch((err) => {
-            console.error(`Hubo un error en la petición ${err}`)
-        })
+            .then((data) => {
+                console.log(data.data.data) // La respuesta en json se llamará "data" e imprimímos la data
+                setCryptos(data.data.data)
+            })
+            .catch((err) => {
+                console.error(`Hubo un error en la petición ${err}`)
+            })
     }, []); // El array vacío significa que se ejecute solo una vez+
 
-    if(!Cryptos) return <span>Cargando datos...</span>
+    if (!Cryptos) return <span className="flex justify-center h-screen bg-blue-100">Cargando datos...</span>
 
     return (
-        <div className="pl-20 pr-5">
+        <div className="pl-20 pr-5 bg-blue-100">
             <h1 className="text-center text-2xl pb-4">Las 100 Cryptomonedas más famosas</h1>
-            <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-7">
+            <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-7">
                 {
                     Cryptos.map(({ id, symbol, name, priceUsd, changePercent24Hr }) => (
-                    //  <CardCrypto nombreCualquiera = { propiedad del JSON } />
-                    //  El nombreCualquiera debe estar igual en el componente CardCrypto (CardCrypto.jsx en las props)
+                        //  <CardCrypto nombreCualquiera = { propiedad del JSON } />
+                        //  El nombreCualquiera debe estar igual en el componente CardCrypto (CardCrypto.jsx en las props)
                         <CardCrypto key={id} symbol={symbol} name={name} priceUSD={priceUsd} changePercent={changePercent24Hr} id={id} />
                     ))
                 }
--- src/pages/Cryptomoneda.jsx
+++ src/pages/Cryptomoneda.jsx
@@ -1,13 +1,38 @@
-import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Cryptomoneda = () => {
     // El hook useParams nos devuelve los parametros que existen en la URL y esos parametros lo definimos en las rutas ":id"
     const params = useParams()
+    const API_URL = import.meta.env.VITE_API_URL;
+    const [info, setInfo] = useState();
+
+    useEffect(() => {
+        axios.get(`${API_URL}assets/${params.id}`)
+            .then((data) => {
+                console.log(data.data.data)
+                setInfo(data.data.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, []);
+
+    if (!info) return <span className="flex justify-center h-screen bg-blue-100">Cargando datos...</span>
 
     return (
-        <div className="pl-20">
-            <h1>Soy la cryptomoneda... {params.id}</h1>
-        </div>
+        <>
+            <div className="pl-20 flex flex-col items-center bg-blue-100 h-screen">
+                <h1>Cryptomoneda "{params.id.toUpperCase()}"</h1>
+                {
+                    info.map(({ id, marketCapUsd, maxSupply, name, priceUsd, rank, supply, symbol, volumeUsd24Hr, vwap24Hr }) => (
+                        <div>{id}</div>
+                    ))
+                }
+                <Link to="/cryptomonedas" className="bg-blue-500 p-4 rounded-lg hover:bg-blue-400 duration-300 text-white">Volver atrás</Link>
+            </div>
+        </>
     )
 };
 
