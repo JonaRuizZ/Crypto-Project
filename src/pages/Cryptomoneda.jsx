7314d5fe478e9f55d67c166fe2d28e0d6aa4b3fe
Jonathan Ruiz <jonathan.ruizz@hotmail.com>
2022-11-25 01:18:18 -0300

    Para poder mostrar los datos que llegaban no usamos .map, utilizamos un JSON.stringify(info) y un JSON.stringify(history) para mostrar respectivamente la información de la criptomoneda y además para mostrar los datos del historial de la misma criptomoneda

--- src/pages/Cryptomoneda.jsx
+++ src/pages/Cryptomoneda.jsx
@@ -6,11 +6,12 @@ const Cryptomoneda = () => {
     // El hook useParams nos devuelve los parametros que existen en la URL y esos parametros lo definimos en las rutas ":id"
     const params = useParams()
     const API_URL = import.meta.env.VITE_API_URL;
-    const [info, setInfo] = useState();
+    const [info, setInfo] = useState({});
+    const [history, setHistory] = useState([]);
 
     useEffect(() => {
         axios.get(`${API_URL}assets/${params.id}`)
-            .then((data) => {
+            .then(data => {
                 console.log(data.data.data)
                 setInfo(data.data.data)
             })
@@ -19,18 +20,73 @@ const Cryptomoneda = () => {
             })
     }, []);
 
-    if (!info) return <span className="flex justify-center h-screen bg-blue-100">Cargando datos...</span>
+    useEffect(() => {
+        axios.get(`${API_URL}assets/${params.id}/history?interval=d1`)
+            .then(data => {
+                console.log(data.data.data)
+                setHistory(data.data.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, []);
+
+    if (!info || !history) return <span className="flex justify-center h-screen bg-blue-100">Cargando datos...</span>
 
     return (
         <>
-            <div className="pl-20 flex flex-col items-center bg-blue-100 h-screen">
+            <div className="pl-20 flex flex-col items-center bg-blue-100 h-full">
                 <h1>Cryptomoneda "{params.id.toUpperCase()}"</h1>
-                {
-                    info.map(({ id, marketCapUsd, maxSupply, name, priceUsd, rank, supply, symbol, volumeUsd24Hr, vwap24Hr }) => (
-                        <div>{id}</div>
-                    ))
-                }
-                <Link to="/cryptomonedas" className="bg-blue-500 p-4 rounded-lg hover:bg-blue-400 duration-300 text-white">Volver atrás</Link>
+                <div>
+                    {
+                        // Como hemos declarado un objeto en el useState, lo pasamos a texto plano con el JSON.stringify
+                        // JSON.stringify(info)
+                    }
+                    <ul>
+                        <li>
+                            <span>ID: {info.id}</span>
+                        </li>
+                        <li>
+                            <span>Rank: {info.rank}</span>
+                        </li>
+                        <li>
+                            <span>Symbol: {info.symbol}</span>
+                        </li>
+                        <li>
+                            <span>Name: {info.name}</span>
+                        </li>
+                        <li>
+                            <span>Supply: {info.supply}</span>
+                        </li>
+                        <li>
+                            <span>maxSupply: {info.maxSupply}</span>
+                        </li>
+                        <li>
+                            <span>marketCapUsd: {info.marketCapUsd}</span>
+                        </li>
+                        <li>
+                            <span>volumeUsd24Hr: {info.volumeUsd24Hr}</span>
+                        </li>
+                        <li>
+                            <span>priceUsd: {info.priceUsd}</span>
+                        </li>
+                        <li>
+                            <span>changePercent24Hr: {info.changePercent24Hr}</span>
+                        </li>
+                        <li>
+                            <span>vwap24Hr: {info.vwap24Hr}</span>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <span>History</span>
+                        </li>
+                        <li>
+                            <span>{JSON.stringify(history)}</span>
+                        </li>
+                    </ul>
+                </div>
+                <Link to="/cryptomonedas" className="bg-blue-500 p-4 rounded-lg hover:bg-blue-400 duration-300 text-white m-4">Volver atrás</Link>
             </div>
         </>
     )
